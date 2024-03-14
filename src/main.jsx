5280b01c9c63db4c308459f3978b192cbfd9a46c
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2024-03-15 01:31:45 +0600

    basic setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Roots from "./componenets/Roots/Roots.jsx";
+import Home from "./componenets/Home/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Roots></Roots>,
+    errorElement: <h2>404 Error</h2>,
+    children: [
+      { path: "/", element: <Home></Home> }
+    ],
   },
 ]);
 
